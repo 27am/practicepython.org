e12a728dacf3cc7244ae582b5a211b1ad069a504
27am <merlina.andrea@gmail.com>
2018-02-26 22:34:22 +0100

    second commit

---
+++ exercise4.py
@@ -0,0 +1,26 @@
+"""
+Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+(If you don’t know what a divisor is, it is a number that divides evenly into another number.
+ For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+"""
+
+def main():
+	a = int(input("Insert a number\n"))
+
+	divisors = []
+
+	for i in range(a):
+
+		try:
+			if a % i == 0:
+				divisors.append(i)
+
+		except ZeroDivisionError:
+			pass
+
+	print(divisors)
+
+
+
+if __name__ == "__main__":
+	main()
